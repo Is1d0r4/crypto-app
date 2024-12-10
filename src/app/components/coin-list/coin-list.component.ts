0dffbac5f39057cc2de04b8e090ebc15a8289183
Isidora Trickovic <trickovic.isidora@gmail.com>
2024-12-10 20:47:27 +0100

    added banner

--- src/app/components/coin-list/coin-list.component.ts
+++ src/app/components/coin-list/coin-list.component.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../service/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-coin-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './coin-list.component.html',
   styleUrl: './coin-list.component.scss',
 })
 export class CoinListComponent implements OnInit {
+  bannerData: any = [];
+
   constructor(private api: ApiService) {}
   ngOnInit() {
     this.getBannerData();
     this.getAllData();
   }
   getBannerData() {
-    this.api.getTrendingCurrency('eur').subscribe((data) => console.log(data));
+    this.api.getTrendingCurrency('eur').subscribe((data) => {
+      console.log(data);
+      this.bannerData = data;
+    });
   }
   getAllData() {
     this.api.getCurrency('eur').subscribe((data) => console.log(data));
